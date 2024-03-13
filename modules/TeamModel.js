291193b36ae738349a3dd1220b884f6c7a632cc1
aadi101204 <113494774+aadi101204@users.noreply.github.com>
2024-03-13 15:54:47 +0530

    modules

---
+++ modules/TeamModel.js
@@ -0,0 +1,70 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../utils/index.js");
+
+const Team = sequelize.define("Team", {
+  team_code: {
+    type: DataTypes.STRING(50),
+    primaryKey: true,
+    unique: true,
+    allowNull: false,
+  },
+  team_name: {
+    type: DataTypes.STRING(100),
+    primaryKey: false,
+    unique: true,
+    allowNull: false,
+  },
+  leader_email: {
+    type: DataTypes.STRING(100),
+    primaryKey: false,
+    unique: true,
+    allowNull: false,
+    references: {
+      model: "User",
+      key: "email_id",
+    },
+  },
+  r0_ppt_link: {
+    type: DataTypes.STRING(300),
+    primaryKey: false,
+    unique: true,
+    allowNull: false,
+  },
+  r2_github_link: {
+    type: DataTypes.STRING(300),
+    primaryKey: false,
+    unique: true,
+    allowNull: false,
+  },
+  r3_ppt_link: {
+    type: DataTypes.STRING(300),
+    primaryKey: false,
+    unique: true,
+    allowNull: false,
+  },
+  score: {
+    type: DataTypes.INTEGER,
+    primaryKey: false,
+    unique: false,
+    allowNull: true,
+  },
+  suggestion: {
+    type: DataTypes.TEXT,
+    primaryKey: false,
+    unique: false,
+    allowNull: false,
+  },
+  r3_order: {
+    type: DataTypes.INTEGER,
+    primaryKey: false,
+    unique: true,
+    allowNull: false,
+  },
+  group_id: {
+    type: DataTypes.STRING(100),
+    primaryKey: false,
+    unique: true,
+    allowNull: false,
+  },
+});
+module.exports = Team;
