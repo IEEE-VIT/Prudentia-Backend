621736e36cced017669d72eaba35930ed60ada0f
Sai Sahith Bonugala <bonugala.saisahith@gmail.com>
2024-03-22 19:36:02 +0530

    ft: github link

--- controller/submissions.controller.js
+++ controller/submissions.controller.js
@@ -49,6 +49,54 @@ const r0 = async (req, res) => {
   }
 };
 
-const submitController = { r0 };
+const r2 = async (req, res) => {
+  try {
+    if (!req.user) {
+      return res.json({ error: "User not found" });
+    }
+
+    const team_code = req.user.team_code;
+
+    if (!team_code) {
+      return res.json({ popup: "You are not part of any team" });
+    }
+
+    const members = await User.findAll({
+      where: { team_code: team_code },
+    });
+
+    if (members.length < 2) {
+      return res.json({
+        popup: "Team must consist of atleast 2 members to submit the link.",
+      });
+    }
+
+    const team = await Team.findByPk(team_code);
+
+    if (team.r2_github_link) {
+      return res.json({ popup: "Link already submitted" });
+    }
+
+    if (req.user.email !== team.leader_email) {
+      return res.json({ popup: "Only the team leader can submit the link" });
+    }
+
+    const { link } = req.body;
+
+    await Team.update(
+      { r2_github_link: link },
+      {
+        where: { team_code: team_code },
+      }
+    );
+
+    res.json({ message: "Link for round 2 submitted successfully" });
+  } catch (error) {
+    console.error("Error submitting link for round 2:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const submitController = { r0, r2 };
 
 module.exports = submitController;
--- routes/submissions.route.js
+++ routes/submissions.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const submitController = require("../controller/submissions.controller");
 
 router.post("/r0", submitController.r0);
-// router.post("/submit/r2", submitController.round2);
+router.post("/r2", submitController.r2);
 // router.post("/submit/r3", submitController.round3);
 
 module.exports = router;
